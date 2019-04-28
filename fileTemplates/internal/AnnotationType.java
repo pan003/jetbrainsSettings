eeafbaf7ee0e6f2c8de69110d1ed0592f9696db0
zhangpan66 <zhangpan6@jd.com>
2019-04-28 20:51:31 +0800

    IU-2019.1 <pan@pandeMacBook-Pro.local Create IntelliLang.xml, project.default.xml, diff.xml, Enum.java, Interface.java, Default.xml, ignore.xml, find.xml, gradle.run.settings.xml, _@user_Darcula.icls, path.macros.xml, mavenVersion.xml, hg.xml, Default.xml, docker.xml, my.icls, editor.codeinsight.xml, grepConsole.xml, keymap.xml, Mybatis_SQL.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, MyGroup.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, CodeGlance.xml, lombok-plugin.xml, git.xml, ui.lnf.xml, filetypes.xml, projectView.xml, Default copy.icls, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, Class.java, jdk.table.xml, codeHelper.xml, mavenRunHelper.xml, intentionSettings.xml, laf.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, material_theme.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, usageView.xml, setterTemplates.xml, sonarlint.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,21 @@
+/**
+  * @copyright: Copyright (c) 2015-2020 jd.com All Rights Reserved
+  * @file: ${NAME}.java project: ${PROJECT_NAME}  
+  * @creator: ${USER}
+  * @date: ${DATE} 
+  */
+  
+  
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+
+/**
+  * ${DESCRIPTION} 
+  * @see:
+  * @author: ${USER}
+  * @version: 1.0
+  * @createdate: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+  * @lastdate:  
+  */
+public @interface ${NAME} {
+}
